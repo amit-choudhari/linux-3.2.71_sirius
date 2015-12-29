b59277cc1b6a0d3d341003145cd98d378cd37cc7
Amit <amitsirius92@gmail.com>
2015-12-29 09:45:35 +0530

    usb pci

--- drivers/char/mychar.c
+++ drivers/char/mychar.c
@@ -1,11 +1,62 @@
-#include <include/linux.h>
-#include <include/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
+#define DEVICE_NAME "temp_char"
+dev_t dev;
+struct cdev *my_cdev;
 
-static int __init hello_init (void)
+int my_open(struct inode *inode, struct file *fp)
+{
+	printk("opened my device\n");
+	return 0;
+}	
+
+int my_release(struct inode *inode, struct file *fp)
+{
+	printk("close my device\n");
+	return 0;
+}	
+
+ssize_t my_read(struct file *fp, char __user *buff,size_t count, loff_t *off )
 {
 
+	return 0;
+}
+
+ssize_t my_write(struct file *fp, char __user *buff,size_t count, loff_t *off )
+{
+
+	*off+=count;
+	return count;
+}
+struct file_operations fops = {
+	.open = my_open,
+	.release = my_release,
+	.read = my_read,
+	.write = my_write,
+
+};
+static int __init hello_init (void)
+{
+	int rc, major, minor;
+
 	printk("inside hello");
+	if(!alloc_chrdev_region(&dev,0,1,DEVICE_NAME)){
+	goto alloc_Fail;
+	}	
+	major=MAJOR(dev);
+	minor=MINOR(dev);
+	
+	printk("device major = %d minor = %d",major,minor);
+	cdev_add(my_cdev, dev, 1);
+	cdev_init(my_cdev,&fops);
+	return 0;
+
+alloc_Fail:
+	printk("char dev alloc failed");
 	return 0;
 }
 
